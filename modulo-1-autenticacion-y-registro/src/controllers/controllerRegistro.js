04513592c1d4ce44d6d608538b3a517d5d17f438
GodBlessedHans <haseveco@gmail.com>
2025-11-19 14:11:31 -0500

    Se crea controller del registro y se implementa validacion

---
+++ modulo-1-autenticacion-y-registro/src/controllers/controllerRegistro.js
@@ -0,0 +1,22 @@
+import { alerta } from "../helpers/alertas.js";
+
+let btnRegister = document.getElementById("btnRegister");
+btnRegister.addEventListener("click", () =>{
+    let correo = document.getElementById("email").value;
+    let contrasena = document.getElementById("password").value;
+    let confirmar = document.getElementById("confirmar").value;
+    let nombre = document.getElementById("nombre").value;
+
+    if (contrasena === confirmar){
+       alerta("Registro exitoso","Se ha completado su registro","success")
+        setTimeout(() => {
+        window.location.href ="https://sweetalert2.github.io/"
+     }, 5000);
+    }
+    else{
+        alerta("Registro fallido","Las contraseñas no coinciden","error")
+    }
+});
+document.getElementById("form-registro").addEventListener("submit", (e) => {
+    e.preventDefault();
+});
